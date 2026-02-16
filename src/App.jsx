961cb59a7c8e31240f3b43339bb1dbaab31d196b
Gye11 <hilallornekk@gmail.com>
2026-02-17 01:35:14 +0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import "./App.css";
+
 import Profile from "./Profile/Profile";
 import FriendList from "./FriendList/FriendList";
 import TransactionHistory from "./TransactionHistory/TransactionHistory";
 
+import userData from "./data/user.json";
+import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
+
 function App() {
   return (
     <>
-      <Profile />
-      <FriendList />
-      <TransactionHistory />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }

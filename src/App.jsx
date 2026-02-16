3975b60c9741e292988a93dde0a45e39fb8386c3
Gye11 <hilallornekk@gmail.com>
2026-02-17 01:19:16 +0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import "./App.css";
-
 import Profile from "./Profile/Profile";
 import FriendList from "./FriendList/FriendList";
 import TransactionHistory from "./TransactionHistory/TransactionHistory";
 
-import userData from "./userData.json";
-import friends from "./friends.json";
-import transactions from "./transactions.json";
-
 function App() {
   return (
     <>
-      <Profile {...userData} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <Profile />
+      <FriendList />
+      <TransactionHistory />
     </>
   );
 }
